b14183bbf832eb147537a4b2947bd779fb61e562
Alexei <motogp1985@gmail.com>
2019-04-29 11:24:32 +0300

    Initial Commit

---
+++ Life Cycle Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Life Cycle Controller
+//
+//  Created by home on 4/29/19.
+//  Copyright © 2019 home. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
